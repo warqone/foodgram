eefa70d8cba61f3de100406a776cc3281e8d231f
Vadim Karimov <warqone12@yandex.ru>
2025-05-17 01:47:30 +0500

    docker compose files created

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -3,6 +3,27 @@ from django.contrib import admin
 from recipes.models import Recipe, Tag, Ingredient
 
 
-admin.site.register(Recipe)
-admin.site.register(Tag)
-admin.site.register(Ingredient)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'pub_date', 'favorites_count')
+    list_filter = ('tags',)
+    search_fields = ('name', 'author__username')
+
+    @admin.display(description='Добавлений в избранное')
+    def favorites_count(self, obj):
+        """Возвращает количество добавлений рецепта в избранное."""
+        return obj.favorite_recipes.count()
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_filter = ('name', 'slug')
+    search_fields = ('name', 'slug')
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
+    search_fields = ('name',)
